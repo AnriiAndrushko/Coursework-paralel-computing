3a876332c0dc20cc4f4d2b87ad50599c874606f5
NEKO-LOVER <andry.i.andrushko@gmail.com>
2023-12-29 14:09:09 +0200

    added ThreadPoolTest

---
+++ Tests/ThreadPoolTest.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Concurrent;
+
+namespace Tests
+{
+    [TestClass]
+    public class ThreadPoolTest
+    {
+        [TestMethod]
+        public void ThreadPool_QueueUserWorkItem_Success()
+        {
+            int currentThreadId = Thread.CurrentThread.ManagedThreadId;
+            int actualThreadId = -1;
+
+            ConcurrentQueue<Task> queue = new ConcurrentQueue<Task>();
+            MyThreadPool pool = new MyThreadPool(queue, 2);
+
+            queue.Enqueue(new Task(() =>
+            {
+                actualThreadId = Thread.CurrentThread.ManagedThreadId;
+            }));
+            pool.Start();
+
+            Thread.Sleep(100);
+
+            Assert.AreNotEqual(currentThreadId, actualThreadId);
+            Assert.AreNotEqual(-1, actualThreadId);
+        }
+    }
+}
