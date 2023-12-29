aa5dbe371e328b82ac13fa597725ed7b29bb4566
NEKO-LOVER <andry.i.andrushko@gmail.com>
2023-12-29 13:46:53 +0200

    added more tests for index

--- Coursework paralel computing/Program.cs
+++ Coursework paralel computing/Program.cs
@@ -2,12 +2,12 @@
 
 using (var index = new InvertedIndexKeeper(6))
 {
-    //index.AddText("hello", "1");
+    index.AddText("hello", "1");
     index.AddText("This, is a sample: document.", "2");
-    //index.AddText("hello cat", "3");
-    //index.AddText("hello hhh", "4");
-    //index.AddText("hello cat", "5");
-    //index.AddText("big cat", "6");
+    index.AddText("hello cat", "3");
+    index.AddText("hello hhh", "4");
+    index.AddText("hello cat", "5");
+    index.AddText("big cat", "6");
     while (index.IsBusy){
         Console.WriteLine("busy");
     }
--- Tests/IndexTest.cs
+++ Tests/IndexTest.cs
@@ -8,13 +8,51 @@ namespace Tests
         {
             InvertedIndex index = new InvertedIndex();
             string documentId = "doc1";
-            string content = "This, is a sample: document.";
+            string content = "This, is a sample: document. ?and& a text;";
 
             index.AddText(content, documentId);
 
             CollectionAssert.Contains(index.GetByWord("sample").ToList(), documentId);
             CollectionAssert.Contains(index.GetByWord("document").ToList(), documentId);
             CollectionAssert.Contains(index.GetByWord("this").ToList(), documentId);
+            CollectionAssert.Contains(index.GetByWord("and").ToList(), documentId);
+            CollectionAssert.Contains(index.GetByWord("text").ToList(), documentId);
+            Assert.AreEqual(1, index.GetByWord("a").Count());
+            Assert.AreEqual(1, index.GetByWord("document").Count());
+        }
+
+        [TestMethod]
+        public void AddDocument_AddsDocumentsToIndex()
+        {
+            InvertedIndex index = new InvertedIndex();
+            string document1Id = "doc1";
+            string content1 = "This, is a sample: document. ?and& a text;";
+            string document2Id = "doc2";
+            string content2 = "this is a document";
+
+            index.AddText(content1, document1Id);
+            index.AddText(content2, document2Id);
+
+            CollectionAssert.Contains(index.GetByWord("document").ToList(), document1Id);
+            CollectionAssert.Contains(index.GetByWord("document").ToList(), document2Id);
+            Assert.AreEqual(1, index.GetByWord("sample").Count());
+            Assert.AreEqual(2, index.GetByWord("document").Count());
+        }
+
+        [TestMethod]
+        public void GetByQuery_GetsDocumentByQuery()
+        {
+            InvertedIndex index = new InvertedIndex();
+            string document1Id = "doc1";
+            string content1 = "This, is a sample: document. ?and& a text;";
+            string document2Id = "doc2";
+            string content2 = "this is a document";
+
+            index.AddText(content1, document1Id);
+            index.AddText(content2, document2Id);
+
+            CollectionAssert.Contains(index.GetByQuery("is a sample").ToList(), document1Id);
+            Assert.AreEqual(1, index.GetByQuery("is a sample").Count());
         }
     }
 }
