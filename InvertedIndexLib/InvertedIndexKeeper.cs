e8ff3428e559311aa0b54f1b5fc38f2a532bc62f
NEKO-LOVER <andry.i.andrushko@gmail.com>
2023-12-29 09:33:34 +0200

    Added InvertedIndexKeeper

---
+++ InvertedIndexLib/InvertedIndexKeeper.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Concurrent;
+
+namespace InvertedIndexLib
+{
+    internal class InvertedIndexKeeper
+    {
+        MyThreadPool _pool;
+        ConcurrentQueue<Task> _queue;
+        InvertedIndex _index;
+
+        public InvertedIndexKeeper(int threadCount)
+        {
+            _queue = new ConcurrentQueue<Task>();
+            _pool = new MyThreadPool(_queue, threadCount);
+            _index = new InvertedIndex();
+            _pool.Start();
+        }
+
+        public void Save(string path)
+        {
+            _index.SaveIndex(path);
+        }
+
+        public void Load(string path)
+        {
+            _pool.Stop();
+            _index = InvertedIndex.LoadIndex(path);
+            _pool.Start();
+        }
+
+        public void AddDoc(string docPath)
+        {
+            _queue.Append(new Task(() => _index.AddDoc(docPath)));
+        }
+
+        public IEnumerable<string> GetByWord(string word)
+        {
+            var tmp = new Task<IEnumerable<string>>(() => _index.GetByWord(word));
+            _queue.Append(tmp);
+            return tmp.Result;
+        }
+
+        public IEnumerable<string> GetByQuery(string sentence)
+        {
+            var tmp = new Task<IEnumerable<string>>(() => _index.GetByQuery(sentence));
+            _queue.Append(tmp);
+            return tmp.Result;
+        }
+    }
+}
---
+++ InvertedIndexLib/MyThreadPool.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Concurrent;
+
+namespace InvertedIndexLib
+{
+    internal class MyThreadPool
+    {
+        Thread[] _pool;
+        ConcurrentQueue<Task> _taskQueue;
+        CancellationTokenSource _cts;
+        private readonly int _size;
+        public int Size => _size;
+
+        public MyThreadPool(ConcurrentQueue<Task> taskQueue, int size = 6)
+        {
+            _size = size;
+            _cts = new CancellationTokenSource();
+            _taskQueue = taskQueue;
+            _pool = new Thread[size];
+            for (int i = 0; i < _pool.Length; i++)
+            {
+                _pool[i] = new Thread(new ThreadStart(worker));
+            }
+        }
+
+        public void Start()
+        {
+            for (int i = 0; i < _pool.Length; i++)
+            {
+                _pool[i].Start();
+            }
+        }
+
+        public void Stop()
+        {
+            _cts.Cancel();
+            for (int i = 0; i < _pool.Length; i++)
+            {
+                _pool[i].Join();
+            }
+        }
+
+        private void worker()
+        {
+            Task curTask;
+            while (!_cts.Token.IsCancellationRequested)
+            {
+
+                if (!_taskQueue.IsEmpty)
+                {
+                    while (!_taskQueue.TryDequeue(out curTask)) { }; //TODO: maybe use monitor here
+                    curTask.RunSynchronously();
+                    continue;
+                }
+            }
+        }
+    }
+}
